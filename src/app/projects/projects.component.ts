ec291c060861b9c8bac74a654a58572512964137
eizarphyo <eizarphyo2601@gmail.com>
2023-10-25 09:50:12 +0630

    added more projects and updated CV

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -48,11 +48,46 @@ export class ProjectsComponent {
         'MongoDB',
         'NodeJS',
         'Express',
-        'AWS - EC2'
+        'Google Cloud - VM instance'
       ],
       isApp: false,
       git: 'https://github.com/eizarphyo/note_taker',
       deployed: 'https://notetakerangular.netlify.app',
+      download: null,
+    },
+    {
+      id: 3,
+      title: 'Note it',
+      desc: 'Typical note taking web app that supports create, edit and delete notes. Connect to MongoDB database with RESTful APIs.',
+      img: '../../assets/images/noteit.png',
+      languages: [
+        'Flutter',
+        'Dart',
+        'MongoDB',
+        'NodeJS',
+        'Express',
+        'Google Cloud - VM instance'
+      ],
+      isApp: true,
+      git: 'https://github.com/eizarphyo/note_app',
+      deployed: null,
+      download: 'https://docs.google.com/uc?export=download&id=1tG82EINzB-SAgLNSb-AXlc-BMjY5S9e9',
+    },
+    {
+      id: 4,
+      title: 'Note API',
+      desc: 'Typical note taking web app that supports create, edit and delete notes. Connect to MongoDB database with RESTful APIs.',
+      img: '../../assets/images/note_api.png',
+      languages: [
+        'NodeJS',
+        'JavaScript',
+        'Express',
+        'MongoDB',
+        'Google Cloud - VM instance'
+      ],
+      isApp: false,
+      git: 'https://github.com/eizarphyo/note_database',
+      deployed: 'https://app.swaggerhub.com/apis/EIZARPHYO2601/Notes/1.0.0',
       download: null,
     },
   ];
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -43,7 +43,7 @@ export class SkillsComponent {
         "MongoDB",
         "PostgresSQL",
         "Firebase",
-        "AWS"
+        "AWS, Google Cloud"
       ]
     }
   ]
